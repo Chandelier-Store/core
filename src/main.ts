d19f01aa8ebedbf90728964683bc1789bc4d9e57
kod1rov.1 <kodirovbehruz54@gmail.com>
2025-09-01 23:02:44 +0300

    fix: removed unused cors settings

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,6 @@ async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
 
 	app.setGlobalPrefix('api')
-	app.enableCors({
-		origin: ['http://localhost:3000'],
-		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-		allowedHeaders: 'Content-Type, Authorization',
-		credentials: true
-	})
 	app.useGlobalPipes(new ValidationPipe({ transform: true }))
 	await app.listen(8080)
 }
